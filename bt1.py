5c32d0043adee142160dbf3e8d7a368d95494d1e
ledanhquan36 <148116017+ledanhquan36@users.noreply.github.com>
2023-12-31 11:23:04 +0700

    Add files via upload

--- bt1.py
+++ bt1.py
@@ -1,5 +1,17 @@
-print("**      **   ********   **        **        *********")
-print("**      **   **         **        **        **     **")
-print("**********   ********   **        **        **     **")
-print("**      **   **         **        **        **     **")
-print("**      **   ********   *******   ********  *********")
+def kiem_tra_tam_giac(a, b, c):
+    if a <= 0 or b <= 0 or c <= 0:
+        return "Độ dài cạnh phải là số dương"
+    elif a + b <= c or a + c <= b or b + c <= a:
+        return "Ba cạnh không thỏa mãn điều kiện của tam giác"
+    else:
+        return "Ba cạnh là độ dài của một tam giác"
+
+try:
+    a = float(input("Nhập độ dài cạnh a: "))
+    b = float(input("Nhập độ dài cạnh b: "))
+    c = float(input("Nhập độ dài cạnh c: "))
+    
+    print(kiem_tra_tam_giac(a, b, c))
+except ValueError:
+    print("Độ dài cạnh phải là một số")
+
